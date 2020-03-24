4200a3574bae358f0189dfa9420f03d176db47c5
falkenca <c.falkenbach@hotmail.com>
2020-03-24 16:02:14 +0100

    Update HelloWorld.java

--- Trying/thing01/thing01/HelloWorld.java
+++ Trying/thing01/thing01/HelloWorld.java
@@ -15,6 +15,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello World");
+		saynt();
+	}
+	public static void saynt() {
+		System.out.println("hello deer");
 	}
 
 }
