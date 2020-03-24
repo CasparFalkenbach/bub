81b5ffb420d243822377a416be706ff23499a1b6
falkenca <c.falkenbach@hotmail.com>
2020-03-24 15:59:51 +0100

    just a test

---
+++ Trying/thing01/thing01/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package thing01;
+
+/**
+ * @author Caspar
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello World");
+	}
+
+}
